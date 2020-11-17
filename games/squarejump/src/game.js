7c0f6dad0e66cefd6825678f8b77ea736933dcfe
BuildTools <unconfigured@null.spigotmc.org>
2020-11-17 19:25:17 +0000

    Initial Commit

---
+++ games/squarejump/src/game.js
@@ -0,0 +1,62 @@
+//Created by 19L Games
+var jsoncode;
+var webdata;
+
+//Get the canvas and 2d graphics context
+var canvas = document.getElementById("canvas");
+var c = canvas.getContext("2d");
+
+
+//controls key variables
+var leftArrowDown = false;
+var rightArrowDown = false;
+
+//Load all the images for the game
+var blanklevel = document.getElementById("blanklevel");
+var level1 = document.getElementById("level1");
+var playerred = document.getElementById("player-red");
+var playerblue = document.getElementById("player-blue");
+var down = document.getElementById("down");
+var currentimg = blanklevel;
+
+
+function setup() {
+	
+    //this is called when the game starts
+    user = new player(50, 300, playerblue, 0.3333, 4);
+
+	jsoncode = getRemote("webdata.json");
+    webdata = JSON.parse(jsoncode);
+    if (webdata.version == "v3.0-beta.4") {
+        console.log("You are running the latest version");
+    } else {
+        ood();
+    }
+     if (webdata.down == false) {
+        console.log("Servers are online");
+    } else {
+        currentimg = down;
+		user.visible = false;
+    }
+	
+    window.addEventListener("keydown", key);
+    window.addEventListener("keyup", keyup);
+    window.setInterval(loop, 1000 / 60);
+    document.getElementById("loading").style = "display:none"
+	document.getElementById("news").innerHTML = webdata.news;
+}
+
+function loop() {
+    //this is the game's main loop
+    clear(currentimg);
+    user.update();
+    user.draw();
+
+}
+
+
+function clear(img) {
+    //this just clears the screen
+    c.drawImage(img, 0, 0);
+
+}
