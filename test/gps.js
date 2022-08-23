dd917446ccdf69cfff062fed341d9c482d484d87
19L-Games <73015630+19L-Games@users.noreply.github.com>
2022-08-23 16:43:20 +0100

    Add files via upload

--- test/gps.js
+++ test/gps.js
@@ -21,6 +21,20 @@ nd.fillStyle = "rgb(0,0,0)";
 pfd.fillRect(0,0,400,400);
 nd.fillRect(0,0,400,400);
 
+text()
+}
+function text() {
+	pfd.fillStyle = nd.fillStyle = "#00ff00";
+	pfd.font = nd.font = "18px Arial";
+	
+	pfd.fillText("alt " + pos.alt , 10,20);
+	pfd.fillText("spd " + pos.spd , 10,40);
+	
+	nd.fillText(pos.lat, 10,20);
+	nd.fillText(pos.lon, 10,40);
+	nd.fillText(pos.hdg,10,80);
+	nd.fillText(pos.acc, 10,100);
+	nd.fillText(pos.t,10,120);
 }
 
 function getLocation() {
